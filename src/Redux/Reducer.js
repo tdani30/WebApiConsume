1966f2d0fd35bb2aae780ca787be93919fda4176
Talha <syedtalha30@gmail.com>
2021-07-06 20:33:08 +0500

    Update Redux

--- src/Redux/Reducer.js
+++ src/Redux/Reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import { User } from "./Action";
 
+
 export const reducers = combineReducers({
     User
 });
--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import { reducers } from "./Reducers";
+import { reducers } from "./Reducer";
 
 
 export const store = createStore(
